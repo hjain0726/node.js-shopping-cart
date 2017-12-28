b845494e7674c0d2568354a47b994df84e807631
hjain0726 <hjain0726@gmail.com>
2017-12-28 15:11:19 +0530

    first commit

---
+++ public/javascripts/checkout.js
@@ -0,0 +1,26 @@
+Stripe.setPublishableKey('pk_test_6XAwA1tfa4yAMBTKLlROPXlU');
+
+var $form =$('#checkout-form');
+$form.submit(function(event){
+  $('#charge-error').addClass('hidden');
+  $form.find('button').prop('disabled',true);
+  Stripe.card.createToken({
+    number:$('#card-number').val(),
+    cvc:$('#card-cvc').val(),
+    exp_month:$('#card-expiry-month').val(),
+    exp_year:$('#card-expiry-year').val(),
+    name:$('#card-name').val()
+  },stripeResponseHandler);
+  return false;
+});
+function stripeResponseHandler(status, response){
+  if(response.error){
+    $('#charge-error').text(response.error.message);
+    $('#charge-error').removeClass('hidden');
+    $form.find('button').prop('disabled',false);
+  }else{
+    var token=response.id;
+    $form.append($('<input type="hidden" name="stripeToken"/>').val(token));
+    $form.get(0).submit();
+  }
+}
---
+++ seed/product-seeder.js
@@ -0,0 +1,48 @@
+var Product=require('../models/product');
+var mongoose=require('mongoose');
+mongoose.connect('mongodb://test:test@ds131687.mlab.com:31687/shopping');
+var products=[
+  new Product({
+  imagePath:'https://upload.wikimedia.org/wikipedia/en/5/5e/Gothiccover.png',
+  title:'Gothic Video Game',
+  description:'Awesome Game!!!!',
+  price:10
+}),
+new Product({
+imagePath:'https://media.phpnuke.org/000/975/756/0bc_350_580_580--world-of-warcraft-1.jpg',
+title:'World of Warcraft Video Game ',
+description:'Also awesome? But of course it was better in vanilla...',
+price:20
+}),
+new Product({
+imagePath:'https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Codbox.jpg/220px-Codbox.jpg',
+title:'Call of Duty Video Game',
+description:'Meh ... nah,it is okay i guess ',
+price:60
+}),
+new Product({
+imagePath:'https://images8.alphacoders.com/882/thumb-1920-882933.jpg',
+title:'Minecraft Video Game',
+description:'Now that is super Awesome',
+price:15
+}),
+new Product({
+imagePath:'https://www.hdwallpapers.in/walls/grand_theft_auto_gta_5-wide.jpg',
+title:'Grand Theft Auto 5',
+description:'Amazing game',
+price:70
+})
+];
+var done=0;
+for(var i=0;i<products.length;i++)
+{
+  products[i].save(function(err,result){
+    done++;
+    if(done===products.length){
+      exit();
+    }
+  });
+}
+function exit(){
+  mongoose.disconnect();
+}
